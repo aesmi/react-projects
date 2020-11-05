c1d5647ef498b78e14e20b6728498f5153ba736c
CuteAegyo <chiuwng10@gmail.com>
2020-11-06 02:43:10 +0800

    proj 5 still needs debugging, missing buttons, starting proj 6-9 tomorrow.

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -3,23 +3,34 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-function App() {
-  const [menuItems, setMenu] = useState(items);
-  const [categories, setCategories] = useState([]);
+//array of all categories within our data array
+//we use a set to remove duplicate categories
+//refer to: https://alligator.io/js/sets-introduction/#:~:text=Sets%20are%20a%20new%20object,like%20object%20literals%20or%20arrays
+const allCategories = ['all', ...new Set(items.map((item) => item.categories))];
 
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+  //util function for filtering categories
   const filterItems = (category) => {
-    const newItems = items.filter((item)=>item.category == category)
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category == category);
+    setMenuItems(newItems);
   }
 
   return (
     <main>
       <section className="menu section">
-        <div className='menu section'>
+        <div className='title'>
           <h2>our menu</h2>
           <div className='underline'></div>
         </div>
-        <Categories />
-        <Menu items={menuItems}/>
+        {/*pass in our util function as a prop'*/}
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
